0e568f4e0ebfc3db4926496389b0c3fc8a5ac077
Pahrul Irfan <ip.5090@gmail.com>
2021-07-29 12:29:54 +0800

    update awal chart

--- application/controllers/admin/Dasboard.php
+++ application/controllers/admin/Dasboard.php
@@ -4,9 +4,11 @@
 
  	  public function index(){
 
+		$data['data'] = $this->db->query("SELECT COUNT(nama_lengkap) as jml, MONTHNAME(tgl_periksa) as bulan FROM `tbl_ibuhamil` GROUP BY month(tgl_periksa)")->result_array();
+
  		$this->load->view('admin/header');
  		$this->load->view('admin/index');
- 		$this->load->view('admin/footer');
+ 		$this->load->view('admin/footer', $data);
  		
  	}
  	   
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,21 +1,21 @@
 <div class="row justify-content-center">
-    <center>
-        <div style="background-image: url('')"> 
-            <h4>Selamat Datang Di Halaman Admin, Selamat Bekerja.</h4>
-            <img src="">
-        </div>
+	<center>
+		<div style="background-image: url('')">
+			<h4>Selamat Datang Di Halaman Admin, Selamat Bekerja.</h4>
+			<img src="">
+		</div>
 
-<!-- Area Chart -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
-                                </div>
-                                <div class="card-body">
-                                    <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
-                                    </div>
-                                    <hr>
-                                    Styling for the area chart can be found in the
-                                    <code>/js/demo/chart-area-demo.js</code> file.
-                                </div>
-                            </div>
+		<!-- Area Chart -->
+		<div class="card shadow mb-4">
+			<div class="card-header py-3">
+				<h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
+			</div>
+			<div class="card-body">
+				<div class="chart-area">
+					<canvas id="myAreaChart"></canvas>
+				</div>
+				<hr>
+				Styling for the area chart can be found in the
+				<code>/js/demo/chart-area-demo.js</code> file.
+			</div>
+		</div>
